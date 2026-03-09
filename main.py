129783e1bed0ce4ad743569746c3488dfee8aed6
Tiganete Alexandru <Alexandru.Tiganete@cognizant.com>
2026-03-09 16:21:16 +0200

    feat: added SQLAlchemy to project

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
 def main():
-    print("Hello from vaultkeep!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
